da9038044b886075b0b870e8010c2b604cd83e38
ConnorWatsonGitHub <66978864+ConnorWatsonGitHub@users.noreply.github.com>
2020-07-23 16:28:48 -0400

    COMP 1073 - Phase Two

---
+++ storageAPI.js
@@ -0,0 +1,35 @@
+function saveTasks(){
+    try {
+    //clearing the storeage before saving new set of tasks
+    window.localStorage.clear();
+    //initializing the text that will be saved in the local storage
+    let textToSave = "";
+    //the DOM element that holds the tasks 
+    let taskList = document.querySelector("ul");
+    //for each task add the name of the task to the textToSave
+    for (let node of taskList.childNodes){
+        //all tasks will be seperated by a delimiter of the '|' character
+        textToSave = textToSave + node.textContent.trim().slice(0, -6) + "|";
+    }
+    //save the Tasks
+    window.localStorage.setItem("tasks", textToSave);
+    //promting the user that the tasks saved successfully
+    promtMsg.className = "success";
+    promtMsg.textContent = "Completed Tasks Successfully Saved to Local Storage";
+
+    }
+    catch(error) {
+        //setting the text in the P element with id error to display the error that occurs
+        promtMsg.className = "error";
+        promtMsg.textContent = "An Error Occured While Saving Tasks to Local Storage: " + error;
+    }
+}
+
+//save the DOM object for the savebtn element
+let saveBtn = document.getElementById("save"); 
+//setting an event listener for the savebtn
+saveBtn.addEventListener("click", saveTasks);
+
+
+
+
